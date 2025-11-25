cb42430650a84c39c787bfd78fc0a6fb08cac3a6
Roberto Jimenez <robertojj2006@gmail.com>
2025-11-25 20:17:21 +0100

    hechois

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def haversine_km(lat1, lon1, lat2, lon2):
     phi1 = math.radians(lat1)
     phi2 = math.radians(lat2)
     dphi = math.radians(lon2 - lon1)
+    dlambda = math.radians(lon2 - lon1)
 
     a = math.sin(dphi/2)**2 + math.cos(phi1) * math.cos(phi2) * math.sin(dlambda/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
